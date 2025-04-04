a3787e6787bede4908c36d368ce09895e78ca17e
unknown <G00428429@atu.ie>
2025-04-04 12:37:01 +0000

    first commit

---
+++ src/app/status/status.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {IonButton, IonRadioGroup,IonRadio,IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton} from '@ionic/angular/standalone';
+
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+  standalone: true,
+  imports: [IonRadioGroup ,IonRadio,IonItem, IonLabel, IonList ,IonBackButton 
+    ,IonButton,IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, 
+    FormsModule]
+})
+export class StatusPage implements OnInit {
+  myStatus: string=' ';
+  constructor(private storage:Storage, private router:Router) { }
+  async onSave()
+  {
+    console.log(this.myStatus);
+    await this.storage.create();
+    await this.storage.set('status', this.myStatus);
+    this.router.navigate(['/home']);
+  }
+
+  async ionViewWillEnter()
+  {
+    await this.storage.create();
+    this.myStatus = await this.storage.get('status');
+  }
+
+
+  ngOnInit() {
+  }
+
+}
